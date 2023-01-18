d4a367e2b108d7b49e136b288acce9c9f4193060
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-18 21:25:05 +0100

    fixes

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 const outerdots = document.querySelector('.outerdots')
+const minutecounts = document.querySelector('.minutecounts')
+let arr = []
 for (let i = 0; i <= 60; i++) {
     outerdots.innerHTML += `<p style="--xx:${i};"></p>`
+
 }
+for (let i = 5; i <= 60; i += 5) {
+    arr.push(i)
+    minutecounts.innerHTML += `<p style="--xxmin:${i};"></p>`
+    if (i % 2 === 0 && i < 60) {
+        minutecounts.innerHTML += `<p style="--xxmin:${i};" class="withoutnum">${i}</p>`
+        console.log(i)
+    }
+}
+console.log(arr.length)
