acf300eba4e83f3980bdacdab8033f9ec28eee78
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-22 16:29:55 +0100

    fixes

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const outerdots = document.querySelector('.outerdots')
-const minutecounts = document.querySelector('.minutecounts')
+const incompletesecs = document.querySelector('.incompletesecs')
+const completesecs = document.querySelector('.completesecs')
+const bigdots = document.querySelector('.bigdots')
 let arr = []
 for (let i = 0; i <= 60; i++) {
     outerdots.innerHTML += `<p style="--xx:${i};"></p>`
 
+}
+
+
+for (let i = 30; i <= 45; i++) {
+    incompletesecs.innerHTML += `<p style="--inc:${i};"></p>`
+
+}
+
+for (let i = 0; i <= 60; i++) {
+    completesecs.innerHTML += `<p style="--comp:${i};"></p>`
+
+}
+
+for (let i = 0; i <= 12; i++) {
+    bigdots.innerHTML += `<p style="--dots:${i};"></p>`
 }
