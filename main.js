59e62b5217eb5b4baa6c07c5fbcc16f8e2ed864a
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-22 14:41:58 +0100

    fixes

--- main.js
+++ main.js
@@ -5,15 +5,3 @@ for (let i = 0; i <= 60; i++) {
     outerdots.innerHTML += `<p style="--xx:${i};"></p>`
 
 }
-let minNumArr = ['', '', 50, '', 40, '', 30, '', 20, '', 10, '']
-minNumArr.reverse()
-for (let i = 0; i < 12; i += 1) {
-    arr.push(i)
-    minutecounts.innerHTML += `<p style="--xxmin:${i};">${minNumArr[i]}</p>`
-    console.log(minNumArr[i])
-    // if (i % 2 === 0 && i < 60) {
-    //     minutecounts.innerHTML += `<p style="--xxmin:${i};"class="withoutnum">${i}</p>`
-    //     console.log(i)
-    // }
-}
-console.log(arr.length)
